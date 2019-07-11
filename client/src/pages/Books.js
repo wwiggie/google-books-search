c5918780d9ebbb468ba260873f15b7e06c5a1f3e
Wendy Wigg <wiggwendy@gmail.com>
2019-07-11 14:10:46 -0700

    delete books from list

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -7,18 +7,53 @@ import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Books extends Component {
+  // Sets our component's initial state
   state = {
-    books: []
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
   };
 
+  // When the component mounts, load all books and save them to this.state.books
   componentDidMount() {
     this.loadBooks();
   }
 
+  // Loads all books and sets them to this.state.books
   loadBooks = () => {
     API.getBooks()
-      .then(res => this.setState({ books: res.data }))
+      .then(res => this.setState({ books: res.data, title: "", author: "", synopsis: "" }))
       .catch(err => console.log(err));
+  };
+
+  // Deletes a book from the database with a given id, then reloads books from the db
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  // Handles updating component state when the user types into the input field
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, use the API.saveBook method to save the book data. Then reload books from the database
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
   };
 
   render() {
@@ -30,10 +65,29 @@ class Books extends Component {
               <h1>What Books Should I Read?</h1>
             </Jumbotron>
             <form>
-              <Input name="title" placeholder="Title (required)" />
-              <Input name="author" placeholder="Author (required)" />
-              <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
-              <FormBtn>Submit Book</FormBtn>
+              <Input 
+                name="title" 
+                placeholder="Title (required)"
+                value={this.state.title}
+                onChange={this.handleInputChange}
+              />
+              <Input 
+                name="author" 
+                placeholder="Author (required)"
+                value={this.state.author}
+                onChange={this.handleInputChange}
+              />
+              <TextArea 
+                name="synopsis" placeholder="Synopsis (Optional)"
+                value={this.state.synopsis}
+                onChange={this.handleInputChange} 
+              />
+              <FormBtn
+                disabled={!(this.state.author && this.state.title)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Book
+              </FormBtn>
             </form>
           </Col>
           <Col size="md-6 sm-12">
@@ -49,7 +103,7 @@ class Books extends Component {
                         {book.title} by {book.author}
                       </strong>
                     </a>
-                    <DeleteBtn />
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
                   </ListItem>
                 ))}
               </List>
