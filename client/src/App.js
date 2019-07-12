864b7a5d427f7a79f51bbdf23385b0f5a9f7d729
Wendy Wigg <wiggwendy@gmail.com>
2019-07-11 19:33:56 -0700

    working app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import logo from "./logo.svg";
 import "./App.css";
@@ -7,10 +10,17 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        <Books />
-      </div>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Books} />
+            <Route exact path="/books" component={Books} />
+            <Route exact path="/books/:id" component={Detail} />
+            <Route component={NoMatch} /> 
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
